d6f09351af5e3efa48918e73e9a2f58d88528de2
FreddieMercy <junhao.zhang.freddie@gmail.com>
2020-12-26 16:05:34 -0800

    Add Practice 1

---
+++ NetBeansPluginPractice1/src/org/freddie/NetBeansPluginPractice1/NetBeansPluginPractice1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.freddie.NetBeansPluginPractice1;
+
+/**
+ *
+ * @author junhaozhang
+ */
+@OnShowing
+public class NetBeansPluginPractice1 extends ModuleInstall implements Runnable {
+
+}
